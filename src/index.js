7b94df3188d3f8891a9681095cce457c06e12177
MarceloRocha-SP <113369232+MarceloRocha-SP@users.noreply.github.com>
2023-05-06 10:41:51 -0300

    Resolução do Desafio Quiz Prompt()

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('oi');
+// Desafio 1
+
+var digiteUmNumero = prompt("Digite um número:");
+var dobroDoNumero = digiteUmNumero * 2;
+
+alert(dobroDoNumero);
+
+// Desafio 2
+
+var valorDaConta = prompt("Digite o valor da conta:");
+var taxaGarcon = 10
+var valorConta = parseFloat(valorDaConta);
+var totalConta = valorConta + (valorDaConta * (taxaGarcon / 100));
+
+alert(totalConta);
+
+// Desafio 3
+
+var digiteValorConta = prompt("Digite o valor da conta:");
+var quantidadeClientes = prompt("Digite a quantidade de clientes:");
+var valorPorCliente = digiteValorConta / quantidadeClientes;
+
+alert(valorPorCliente);
